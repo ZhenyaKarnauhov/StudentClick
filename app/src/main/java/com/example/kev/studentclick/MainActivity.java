3fa57c996aa5a4b9a99751461a4cbd545d623a57
Евгений Карнаухов <ekarnauho@mail.ru>
2018-09-20 15:10:36 +0300

    Счётчик студентов

---
+++ app/src/main/java/com/example/kev/studentclick/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.kev.studentclick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Integer counter =0;
+    private String TAG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickBtnAddStudents(View view) {
+        counter++;
+        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+
+    }
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
+        Log.d(TAG, "onSaveInstanceState" );
+    }
+    protected void onRestoreInstanceState( Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.containsKey("counter")) {
+            counter = savedInstanceState.getInt("counter");
+
+        }
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+
+    public void ZeroButton(View view) {
+        counter = 0;
+        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+
+    }
+}
